9d3d00d7e3c97670a39b05533d5adbc6be946a30
Giovanna-Biasoli <giovannabiasoli@hotmail.com>
2022-09-19 21:38:09 -0300

    commit aula

---
+++ Aula.js
@@ -0,0 +1,27 @@
+class MousseMaracuja {
+
+    constructor(leiteMoca, cremeDeleite, maracuja) {
+
+        this.leiteMoca = leiteMoca
+        this.cremeDeleite = cremeDeleite
+        this.maracuja = maracuja
+    }
+
+    fazerMousse(){
+        
+        if(this.leiteMoca == 2, this.cremeDeleite == 2, this.maracuja == 2){
+
+
+            console.log ("mouse feito :)")
+        }
+        else{
+
+
+             console.log ("ingredientes nao suficientes :(")
+        }
+    }
+} 
+
+const Mousse = new MousseMaracuja( 1, 1, 2 )
+Mousse.fazerMousse()
+
